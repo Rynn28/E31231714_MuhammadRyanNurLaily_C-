a652613e2ba91bb8b3b9b2bc8040bf74cbed9213
Rynn28 <158027047+Rynn28@users.noreply.github.com>
2024-01-29 15:15:45 +0700

    Create Lingkaran.java

---
+++ Lingkaran.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Asus
+ */
+public class Lingkaran {
+    private static final double radius = 7;
+ public static void main(String[] args) {
+ double keliling= 2* Math.PI * radius ;
+ double luas = Math.PI * radius * radius;
+ System.out.println("Jari-jari = " + radius);
+ System.out.println("Keliling = " + keliling);
+ System.out.println("Luas = " + luas);
+}
+}
